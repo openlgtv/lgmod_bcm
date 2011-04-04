ed6ebd2188ec5e3923a3da4cb7aa54482fe80df1
xeros <xeros>
2011-04-04 17:12:43 +0000

    OpenLGTV BCM: fix nanddump cmdline in backup.sh script

--- OpenLGTV_BCM-src/scripts/backup.sh
+++ OpenLGTV_BCM-src/scripts/backup.sh
@@ -2,18 +2,25 @@
 if [ "$1" != "" ]
 then
     back_dir=$1
-    cur_dir=`pwd`
-    mkdir -p $back_dir
-    cd $back_dir
-    for i in `cat /proc/mtd | grep -v erasesize | awk '{print $1 "_" $4}' | sed 's/\"//g' | sed 's/://g'`
-    do
-	echo Making standard backup of $i ...
-	cat /dev/`echo $i | sed 's/_.*//g'` > $back_dir/$i
-	echo Making nanddump backup of $i ...
-	nanddump -a -f $back_dir/$i.nand /dev/`echo $i | sed 's/_.*//g'`
-    done
-    echo Making NVRAM copy ...    
-    cp -f /tmp/nvram $back_dir/
-    echo Done.
-    cd $cur_dir
+else
+    back_dir=/mnt/usb1/Drive1
 fi
+
+echo "OpenLGTV BCM-INFO: Backup script running with \"$1\" argument..."
+
+cur_dir=`pwd`
+mkdir -p $back_dir
+cd $back_dir
+#cat /proc/mtd | sed 's/mtd\(.\):/mtd0\1:/'
+#for i in `cat /proc/mtd | grep -v erasesize | awk "BEGIN {printf \"mtd%02d_%s\", \"$1\", \"$4\"}" | sed 's/\"//g' | sed 's/://g'`
+for i in `cat /proc/mtd | grep -v erasesize | awk '{print $1 "_" $4}' | sed 's/\"//g' | sed 's/mtd\(.\):/mtd0\1/'`
+do
+    echo "Making standard backup of $i ..."
+    cat /dev/`echo $i | sed 's/_.*//g' | sed 's/mtd0/mtd/g'` > $back_dir/$i
+    echo "Making nanddump backup of $i ..."
+    nanddump -f $back_dir/$i.nand /dev/`echo $i | sed 's/_.*//g' | sed 's/mtd0/mtd/g'`
+done
+echo "Making NVRAM copy ..."
+cp -f /tmp/nvram $back_dir/
+echo "Backup DONE."
+cd $cur_dir
