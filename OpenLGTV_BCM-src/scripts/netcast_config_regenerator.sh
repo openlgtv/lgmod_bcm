e3101503ea218c85b0ee76b7cfcfb2ef0733cd6b
xeros <xeros>
2011-04-08 09:54:36 +0000

    OpenLGTV BCM: make id_name and id_number variables for netcast_config_regenerator.sh

--- OpenLGTV_BCM-src/scripts/netcast_config_regenerator.sh
+++ OpenLGTV_BCM-src/scripts/netcast_config_regenerator.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
-# OpenLGTV BCM NetCast config parser and regenerator
+# OpenLGTV BCM NetCast config parser and regenerator v.0.1
 echo "OpenLGTV BCM-INFO: NetCast config parser and regenerator script."
 if [ "$1" != "" ]
 then
+    id_number=254
+    id_name=openlgtv
     org_cfgxml=$1
     tmp_cfgxml=$org_cfgxml.tmp
     new_cfgxml=$org_cfgxml.new
@@ -26,14 +28,14 @@ then
 	if [ "$2" = "add_openlgtv" ]
 	then
 	    echo "OpenLGTV BCM-INFO: NetCast config generator: \"add_openlgtv\" argument passed..."
-	    if [ -z "`grep openlgtv $org_cfgxml`" ]
+	    if [ -z "`grep $id_name $org_cfgxml`" ]
 	    then
-		echo "OpenLGTV BCM-INFO: NetCast config generator: adding \"openlgtv\" id to exitsting config.xml"
-		cat $org_cfgxml | sed 's#<country code=\(.*\)#<country code=\1\n\t\t\t\t\t<item id=\"openlgtv\" type=\"browser\" use_magic=\"true\" check_network=\"false\" resolution=\"1280*720\" use_com_ani=\"false\" mini_ver=\"\" >\r\n\t\t\t\t\t\t\t\t<exec_engine>/mnt/browser/run3556</exec_engine>\r\n\t\t\t\t\t\t\t\t<exec_app>254</exec_app>\r\n\t\t\t\t\t</item>\r\n#g' > $new_cfgxml
+		echo "OpenLGTV BCM-INFO: NetCast config generator: adding \"$id_name\" id to exitsting config.xml"
+		cat $org_cfgxml | sed "s#<country code=\(.*\)#<country code=\1\n\t\t\t\t\t<item id=\"$id_name\" type=\"browser\" use_magic=\"true\" check_network=\"false\" resolution=\"1280*720\" use_com_ani=\"false\" mini_ver=\"\" >\r\n\t\t\t\t\t\t\t\t<exec_engine>/mnt/browser/run3556</exec_engine>\r\n\t\t\t\t\t\t\t\t<exec_app>$id_number</exec_app>\r\n\t\t\t\t\t</item>\r\n#g" > $new_cfgxml
 		mv $org_cfgxml $bck_cfgxml
 		mv $new_cfgxml $org_cfgxml
 	    else
-		echo "OpenLGTV BCM-INFO: NetCast config generator: \"openlgtv\" id already exist in current config.xml"
+		echo "OpenLGTV BCM-INFO: NetCast config generator: \"$id_name\" id already exist in current config.xml"
 	    fi
 	fi
 	if [ "$3" != "" ]
@@ -41,16 +43,18 @@ then
 	    org_brw_app_txt=$3
 	    new_brw_app_txt=$3.new
 	    bck_brw_app_txt=$3.backup
-	    is_id_openlgtv="`grep ^254 $org_brw_app_txt | grep \"http://127.0.0.1/\"`"
+	    #is_id_openlgtv="`grep ^254 $org_brw_app_txt | grep \"http://127.0.0.1/\"`"
+	    is_id_openlgtv="`grep ^$id_number $org_brw_app_txt | grep \"http://127.0.0.1/\"`"
 	    if [ -z "$is_id_openlgtv" ]
 	    then
-		echo "OpenLGTV BCM-INFO: NetCast config generator: adding \"openlgtv\" id 254 to exitsting browser_application.txt file"
+		echo "OpenLGTV BCM-INFO: NetCast config generator: adding \"$id_name\" id $id_number to exitsting browser_application.txt file"
 		cp -f $org_brw_app_txt $new_brw_app_txt
-		echo '254\thttp://127.0.0.1/\t1\t1\t1\t1\t1\t1\t0\ten\t1\t1\t1\t1\r\n' >> $new_brw_app_txt
+		#echo '254\thttp://127.0.0.1/\t1\t1\t1\t1\t1\t1\t0\ten\t1\t1\t1\t1\r\n' >> $new_brw_app_txt
+		echo "$id_number\thttp://127.0.0.1/\t1\t1\t1\t1\t1\t1\t0\ten\t1\t1\t1\t1\r\n" >> $new_brw_app_txt
 		mv $org_brw_app_txt $bck_brw_app_txt
 		mv $new_brw_app_txt $org_brw_app_txt
 	    else
-		echo "OpenLGTV BCM-INFO: NetCast config generator: \"openlgtv\" id 254 already exist in current browser_application.txt"
+		echo "OpenLGTV BCM-INFO: NetCast config generator: \"$id_name\" id $id_number already exist in current browser_application.txt"
 	    fi
 	fi
     fi
