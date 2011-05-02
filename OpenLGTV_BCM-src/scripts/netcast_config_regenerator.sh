dbda05451fdb5167fbf19edb312793bc1382b8b6
xeros <xeros>
2011-05-02 06:14:52 +0000

    OpenLGTV BCM: netcast_config_regenerator.sh fix for enable_all parameter

--- OpenLGTV_BCM-src/scripts/netcast_config_regenerator.sh
+++ OpenLGTV_BCM-src/scripts/netcast_config_regenerator.sh
@@ -14,18 +14,21 @@ then
     if [ "$2" = "enable_all" ]
     then
 	echo "OpenLGTV BCM-INFO: NetCast config generator: \"enable_all\" argument passed, regenerating new config.xml: $org_cfgxml with all services"
-	cat $org_cfgxml | sed 's/\r//g' | sed 's/<!--//g' | sed 's/-->//g' | sed 's/^ *//g' | sed 's/^\t*//g' | grep -v "^$" | sed 's/^ *//g' | sed 's/>j$/>/g' | grep -v '<.*xml>' | grep -v '<.*country.*>' | tr -d '\n' | sed 's#</item><item#</item>\n<item#g' | sed 's/\"\([a-zA-Z]*\)=/\" \1=/g' | sort | uniq > $tmp_cfgxml
+	cat $org_cfgxml | tr -d '\r' | sed -e 's/<!--//g' -e 's/-->//g' | sed -e 's/^ *//g' -e 's/^\t*//g' | grep -v "^$" | sed -e 's/^ *//g' | sed -e 's/>j$/>/g' | grep -v '<.*xml>' | grep -v '<.*country.*>' | tr -d '\n' | sed -e 's#</item><item#</item>\n<item#g' | sed -e 's/\"\([a-zA-Z]*\)=/\" \1=/g' | sort | uniq > $tmp_cfgxml
 	echo -e '<xml>\r' > $new_cfgxml
 	#for cntry in `cat $org_cfgxml | grep 'country code=' | awk -F\" '{print $2}' | sort | uniq`
 	for cntry in `cat $org_cfgxml | grep 'country code=' | awk -F\" '{print $2}' | grep COMMON | sort | uniq`
 	do
 	    echo -e "\t<country code=\"$cntry\">\r" >> $new_cfgxml
-	    cat $tmp_cfgxml | sed 's/^/\t\t/g'| sed 's/></>\r\n\t\t\t</g' | sed 's#\t</item>.*#</item>\r#g' >> $new_cfgxml
+	    # v- busybox sed has problems with handling '\r'
+	    #cat $tmp_cfgxml | sed -e 's/^/\t\t/g'| sed -e 's/></>\r\n\t\t\t</g' -e 's#\t</item>.*#</item>\r#g' >> $new_cfgxml
+	    cat $tmp_cfgxml | sed -e 's/^/\t\t/g'| sed -e 's/></>\n\t\t\t</g' -e 's#\t</item>.*#</item>#g' >> $new_cfgxml
 	    echo -e "\t</country>\r" >> $new_cfgxml
 	    echo -e "\r" >> $new_cfgxml
 	done
 	echo -e "</xml>\r" >> $new_cfgxml
-	rm -f $tmp_cfgxml
+	#rm -f $tmp_cfgxml
+	mv -f $new_cfgxml $org_cfgxml
     else
 	if [ "$2" = "add_openlgtv" ]
 	then
