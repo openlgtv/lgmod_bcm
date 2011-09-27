8a39cbfbea64a699f2a7b4d93b45ea85e5337533
xeros <xeros>
2011-09-27 12:54:43 +0000

    OpenLGTV BCM: make mtdinfo.sh accept path to partition or partition image file argument

--- OpenLGTV_BCM-src/scripts/mtdinfo.sh
+++ OpenLGTV_BCM-src/scripts/mtdinfo.sh
@@ -48,5 +48,12 @@
 #those lines are cut:
 #................................00000000 00000000 ................................       0        0        0
 
+if [ -n "$1" ]
+then
+    mtdinfo="$1"
+else
+    mtdinfo="/dev/mtd1"
+fi
+
 echo "MTDINFO:"
-hexdump /dev/mtd1 -vs240 -e'32 "%_p" 2 "%08x "" " 32 "%_p" "%8d " 2 "%8x ""\n"' | head -n 60 | grep -v "^\."
+hexdump $mtdinfo -vs240 -e'32 "%_p" 2 "%08x "" " 32 "%_p" "%8d " 2 "%8x ""\n"' | head -n 60 | grep -v "^\."
