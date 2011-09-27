303efe6d29dd1616e3b3adcc831a67a741436dce
xeros <xeros>
2011-09-27 06:48:28 +0000

    OpenLGTV BCM: attempt to fix netcast_config_check.sh - busybox egrep applet behaves differently linked with glibc and uclibc

--- OpenLGTV_BCM-src/scripts/netcast_config_check.sh
+++ OpenLGTV_BCM-src/scripts/netcast_config_check.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# OpenLGTV BCM NetCast config format check v.0.0.1 by xeros
+# OpenLGTV BCM NetCast config format check v.0.0.2 by xeros
 # Source code released under GPL License
 
 # busybox egrep applet works exactly the same as grep - need to fix that
@@ -15,7 +15,8 @@ export tmp_cfgxml="/tmp/config.xml"
 cat $1 | tr -d '\r\n' > $tmp_cfgxml
 
 #if [ -z "`grep '<xml>' $config_xml`" -o -z "`grep '<country' $config_xml`" -o -z "`grep '<item' $config_xml`" ]
-if [ -z "`egrep -e '<xml>.*<country.*<item.*</item>.*</country>.*</xml>' $tmp_cfgxml`" ]
+#if [ -z "`egrep -e '<xml>.*<country.*<item.*</item>.*</country>.*</xml>' $tmp_cfgxml`" ]
+if [ -z "`awk '/<xml>.*<country.*<item.*<\/item>.*<\/country>.*<\/xml>/{print}' $tmp_cfgxml`" ]
 then
     echo "Input file is not proper config.xml"
     export config_ver=0
@@ -23,9 +24,11 @@ then
 fi
 
 #if [ -n "`grep '<exec_engine>' $config_xml`" -a -n "`grep '<exec_app>' $config_xml`" -a -n "`grep 'check_network' $config_xml`" -a -n "`grep 'native' $config_xml`" ]
-if [ -n "`egrep -e 'native.*check_network.*<exec_engine>.*<exec_app>' $tmp_cfgxml`" ]
+#if [ -n "`egrep -e 'native.*check_network.*<exec_engine>.*<exec_app>' $tmp_cfgxml`" ]
+if [ -n "`awk '/native.*check_network.*<exec_engine>.*<exec_app>/{print}' $tmp_cfgxml`" ]
 then
-    if [ -n "`egrep -e '<title>.*<cp_main_path>' $tmp_cfgxml`" ]
+    #if [ -n "`egrep -e '<title>.*<cp_main_path>' $tmp_cfgxml`" ]
+    if [ -n "`awk '/<title>.*<cp_main_path>/{print}' $tmp_cfgxml`" ]
     then
 	# BCM35230 SmartTV
 	export config_ver=3
@@ -34,7 +37,8 @@ then
         export config_ver=2
     fi
 else
-    if [ -n "`egrep -e '<title>.*<url_exec>.*<url_icon>' $tmp_cfgxml`" ]
+    #if [ -n "`egrep -e '<title>.*<url_exec>.*<url_icon>' $tmp_cfgxml`" ]
+    if [ -n "`awk '/<title>.*<url_exec>.*<url_icon>/{print}' $tmp_cfgxml`" ]
     then
 	# BCM3549/3556 old
         export config_ver=1
