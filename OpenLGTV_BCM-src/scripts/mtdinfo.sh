7747d5a08f1832e1813396884642bc610aaa9f2e
xeros <xeros>
2011-09-27 20:39:16 +0000

    OpenLGTV BCM: fix autoupgrade from usb stick/disk, mtdinfo improvement (code by mmm4m5m), fix netcast_config_check.sh (problem was with regex search on big line lenght)

--- OpenLGTV_BCM-src/scripts/mtdinfo.sh
+++ OpenLGTV_BCM-src/scripts/mtdinfo.sh
@@ -56,4 +56,5 @@ else
 fi
 
 echo "MTDINFO:"
-hexdump $mtdinfo -vs240 -e'32 "%_p" 2 "%08x "" " 32 "%_p" "%8d " 2 "%8x ""\n"' | head -n 60 | grep -v "^\."
+#hexdump $mtdinfo -vs240 -e'32 "%_p" 2 "%08x "" " 32 "%_p" "%8d " 2 "%8x ""\n"' | head -n 60 | grep -v "^\."
+hexdump $mtdinfo -vs240 -e'32 "%_p" " %08x ""%08x " 32 "%_p" " %8d"" %8x " /1 "Uu:%x" /1 " %x " /1 "CIMF:%x" /1 " %x" "\n"' | head -n 60 | grep -v "^\."
