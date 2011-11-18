840f19de97d1751f876ab3486874f0b4bdbe0595
xeros <xeros>
2011-11-18 19:12:37 +0000

    OpenLGTV BCM: mark version as 0.5.0-beta (althrough it's not yet completely finished for beta) to make drop depreciated settings and do other actions set for upgrade detection - settings and installation parameters should now stay in beta as they are in current code, without change

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# OpenLGTV BCM 0.5.0-pre-beta installation script v.1.91 by xeros
+# OpenLGTV BCM 0.5.0-beta installation script v.1.92 by xeros
 # Source code released under GPL License
 
 # it needs $file.sqf and $file.sha1 files in the same dir as this script
@@ -30,7 +30,7 @@ fi
 # forced rebooting option disabled for manual upgrade/installations
 #rebooting=0
 
-ver=0.5.0-pre-beta
+ver=0.5.0-beta
 supported_rootfs_ver="V1.00.51 Mar 01 2010"
 supported_rootfs_ver2011="V1.00.18 Jan 10 2011"
 development=1
@@ -180,7 +180,7 @@ sleep 1
 
 echo "" | tee -a $log
 date 2>&1 | tee -a $log
-echo "OpenLGTV BCM 0.5.0-pre-beta installation script for $platform platform by xeros" | tee -a $log
+echo "OpenLGTV BCM 0.5.0-beta installation script for $platform platform by xeros" | tee -a $log
 
 #if [ "$2" = "autoupgrade" ]
 if [ "autoupgrade" = "1" ]
