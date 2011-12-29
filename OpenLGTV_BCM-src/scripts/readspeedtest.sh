215e9e17b1e3f53938023b63616e83474cb99a27
xeros <xeros>
2011-12-29 11:39:49 +0000

    OpenLGTV BCM: mount options change for USB storage partitions

--- OpenLGTV_BCM-src/scripts/readspeedtest.sh
+++ OpenLGTV_BCM-src/scripts/readspeedtest.sh
@@ -1,10 +1,14 @@
 #!/bin/sh
+# Source code released under GPL License
+# readspeedtest.sh script for Saturn6/Saturn7/BCM by mmm4m5m,xeros
+
 f="$1"
-if [ -f "$f" ]
-then
-	time cat "$f" 2>&1 >/dev/null | awk -F'[^.0-9]+' '/^r/{s='$(stat -c%s "$f")';t=$2*60+$3;print s" / "t" = "s/t/1024/1024" MB/s"}'
+if [ -f "$f" ]; then
+	size=$(stat -c%s "$f")
+	time cat "$f" 2>&1 >/dev/null | \
+		awk -F'[^.0-9]+' '/^r/{s='"$size"';t=$2*60+$3;print s" / "t" = "s/t/1024/1024" MB/s"}'
 else
-	echo "Script for file read transfer check by mmm4m5m"
-	echo "Measurement will be accurate if run once per file and file size should be at least 100MB"
+	echo "OpenLGTV: Script for file read transfer check"
 	echo "Usage: $0 /path/to/file"
+	echo "For accurate results: run once per file, use file with min size 100MB"
 fi
