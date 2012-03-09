5e6536fb9471d5da0a2f1f0b7d961edd2cee33fd
xeros <xeros>
2012-03-09 13:35:45 +0000

    OpenLGTV BCM: icons_download.sh should now handle download and unpacking errors and try to download/unpack just 10 times, not forever - still need to think more about it

--- OpenLGTV_BCM-src/scripts/icons_download.sh
+++ OpenLGTV_BCM-src/scripts/icons_download.sh
@@ -15,44 +15,51 @@ imd5="7c19bece3f7b27cdf9196b07868026f2"
 useragent="Mozilla/5.0 (X11; Linux x86; rv:10.0.2) Gecko/20100101 Firefox/10.0.2"
 #ilinks_count=3
 ilinks_count=2
+unpacked_ok=0
+try_count=0
 
 if [ ! -d "$icons_dir" -o "$uver" != "$ver" -o ! -f "$icons_dir/unknown.png" ]
 then
     echo "OpenLGTV_BCM-INFO: icons_download.sh: OpenLGTV BCM upgrade/downgrade detected - downloading currently supported icons..."
-    while [ -z "$PING_OK" ]
+    # TODO: check logic
+    while [ "$unpacked_ok" -eq 0 -o "$try_count" -lt 10 ]
     do
-	rnd=$RANDOM
-	let "rnd %= $ilinks_count"
-	case $rnd in
-	0)
-	    ilink="$ilink1";;
-	1)
-	    ilink="$ilink2";;
-	*)
-	    ilink="$ilink3";;
-	esac
-	ihost="`echo $ilink | awk -F/ '{print $3}'`"
-	# ugly workaround for disabled pings on dl.dropbox.com
-	[ "$ihost" = "dl.dropbox.com" ] && ihost="dropbox.com"
-	echo "OpenLGTV_BCM-INFO: icons_download.sh: trying to use download link: $ilink"
-	[ -n "$ihost" ] && PING_OK="`ping -4 -c 1 $ihost 2>/dev/null | grep '64 bytes from'`"
-	sleep 3
+	PING_OK=""
+	while [ -z "$PING_OK" -o "$try_count" -lt 10 ]
+	do
+	    try_count=$((${try_count}+1))
+	    rnd=$RANDOM
+	    let "rnd %= $ilinks_count"
+	    case $rnd in
+	    0)
+		ilink="$ilink1";;
+	    1)
+		ilink="$ilink2";;
+	    *)
+		ilink="$ilink3";;
+	    esac
+	    ihost="`echo $ilink | awk -F/ '{print $3}'`"
+	    # ugly workaround for disabled pings on dl.dropbox.com
+	    [ "$ihost" = "dl.dropbox.com" ] && ihost="dropbox.com"
+	    echo "OpenLGTV_BCM-INFO: icons_download.sh: trying to use download link: $ilink"
+	    [ -n "$ihost" ] && PING_OK="`ping -4 -c 1 $ihost 2>/dev/null | grep '64 bytes from'`"
+	    sleep 3
+	done
+	cur_dir=`pwd`
+	mkdir -p "$icons_dir"
+	cd "$icons_dir"
+	wget -q -c -U "$useragent" "$ilink" -O /tmp/icons.zip
+	md5s="`md5sum /tmp/icons.zip`"
+	if [ "${md5s:0:32}" != "$imd5" ]
+	then
+	    echo "OpenLGTV_BCM-ERROR: icons_download.sh: icons downloaded but zip file checksum mismatch: \"${md5s:0:32}\" != \"$imd5\", retrying to download again..."
+	    #exit 1
+	else
+	    echo "OpenLGTV_BCM-INFO: icons_download.sh: icons downloaded and checksum is OK, unzipping..."
+	    unzip -n /tmp/icons.zip && unpacked_ok=1 || echo "OpenLGTV_BCM-ERROR: icons_download.sh: unzipping failed, retrying to download again..."
+	fi
+	rm -f /tmp/icons.zip
     done
-    cur_dir=`pwd`
-    mkdir -p "$icons_dir"
-    cd "$icons_dir"
-    wget -q -c -U "$useragent" "$ilink" -O /tmp/icons.zip
-    md5s="`md5sum /tmp/icons.zip`"
-    if [ "${md5s:0:32}" != "$imd5" ]
-    then
-	echo "OpenLGTV_BCM-ERROR: icons_download.sh: icons downloaded but zip file checksum mismatch: ${md5s:0:32} != $imd5"
-	rm /tmp/icons.zip
-	exit 1
-    else
-	echo "OpenLGTV_BCM-INFO: icons_download.sh: icons downloaded and checksum is OK, unzipping..."
-	unzip -n /tmp/icons.zip
-	rm /tmp/icons.zip
-    fi
 fi
 cd $cur_dir
 echo "OpenLGTV_BCM-INFO: icons_download.sh: exit"
