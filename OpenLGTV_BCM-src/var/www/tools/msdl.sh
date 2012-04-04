84435d2a939fcc98961d484720980a38f5c2ce8c
xeros <xeros>
2012-04-04 06:35:25 +0000

    OpenLGTV BCM: add simple 4x2 network services menu example for customization

--- OpenLGTV_BCM-src/var/www/tools/msdl.sh
+++ OpenLGTV_BCM-src/var/www/tools/msdl.sh
@@ -3,5 +3,6 @@ cat <<EOF
 Content-type: application/x-netcast-av
 
 EOF
+killall msdl-1.2.7-r2 2>/dev/null
 msdl=`which msdl-1.2.7-r2-orig`
 [ -n "$msdl" ] && "$msdl" -q -o - "$QUERY_STRING" || wget -q -O - "$QUERY_STRING"
